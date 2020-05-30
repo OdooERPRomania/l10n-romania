b08f93ee934d610e6c47b9deaf7cf415ed41f3bc
devCbssolutionsRo <dev@cbssolutions.ro>
2020-05-30 12:21:22 +0300

    k stock_change_standrd_price

--- l10n_ro_stock_account/wizard/__init__.py
+++ l10n_ro_stock_account/wizard/__init__.py
@@ -3,5 +3,5 @@
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
-#import stock_invoice_onshipping
+import stock_invoice_onshipping
 from . import stock_change_standard_price
